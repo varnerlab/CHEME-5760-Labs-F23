6658a18cd7d33dce75b8bf532975e781153181f8
Jeffrey Varner <jdv27@cornell.edu>
2023-09-14 15:11:29 -0400

    Lab 4c done
    lab 4c done

--- src/Compute.jl
+++ src/Compute.jl
@@ -30,3 +30,30 @@ function log_return_matrix(dataset::Dict{Int64,DataFrame},
     # return -
     return return_matrix;
 end
+
+function fractional_return_matrix(dataset::Dict{Int64,DataFrame}, 
+    firms::Array{Int64,1})::Array{Float64,2}
+
+    # initialize -
+    number_of_firms = length(firms);
+    number_of_trading_days = nrow(dataset[1]);
+    return_matrix = Array{Float64,2}(undef, number_of_trading_days-1, number_of_firms);
+
+    # main loop -
+    for i ∈ eachindex(firms) 
+
+        # get the firm data -
+        firm_index = firms[i];
+        firm_data = dataset[firm_index];
+
+        # compute the log returns -
+        for j ∈ 2:number_of_trading_days
+            S₁ = firm_data[j-1, :volume_weighted_average_price];
+            S₂ = firm_data[j, :volume_weighted_average_price];
+            return_matrix[j-1, i] = (S₂ - S₁)/S₁;
+        end
+    end
+
+    # return -
+    return return_matrix;
+end
